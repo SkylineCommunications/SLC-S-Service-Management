80fc0886e8fa7c8d6767322f4ef7c1d802bb94d1
SKYLINE2\RobinME <robin.meurisse@skyline.be>
2026-03-12 15:14:38 +0100

    SLNX + NuGet alignment.

--- SLC_SM_AS_DynamicDelete/SLC_SM_AS_DynamicDelete.cs
+++ SLC_SM_AS_DynamicDelete/SLC_SM_AS_DynamicDelete.cs
@@ -1,46 +1,8 @@
 /*
 ****************************************************************************
-*  Copyright (c) 2025,  Skyline Communications NV  All Rights Reserved.    *
+*  Copyright (c),  Skyline Communications NV  All Rights Reserved.    *
 ****************************************************************************
 
-By using this script, you expressly agree with the usage terms and
-conditions set out below.
-This script and all related materials are protected by copyrights and
-other intellectual property rights that exclusively belong
-to Skyline Communications.
-
-A user license granted for this script is strictly for personal use only.
-This script may not be used in any way by anyone without the prior
-written consent of Skyline Communications. Any sublicensing of this
-script is forbidden.
-
-Any modifications to this script by the user are only allowed for
-personal use and within the intended purpose of the script,
-and will remain the sole responsibility of the user.
-Skyline Communications will not be responsible for any damages or
-malfunctions whatsoever of the script resulting from a modification
-or adaptation by the user.
-
-The content of this script is confidential information.
-The user hereby agrees to keep this confidential information strictly
-secret and confidential and not to disclose or reveal it, in whole
-or in part, directly or indirectly to any person, entity, organization
-or administration without the prior written consent of
-Skyline Communications.
-
-Any inquiries can be addressed to:
-
-	Skyline Communications NV
-	Ambachtenstraat 33
-	B-8870 Izegem
-	Belgium
-	Tel.	: +32 51 31 35 69
-	Fax.	: +32 51 31 01 29
-	E-mail	: info@skyline.be
-	Web		: www.skyline.be
-	Contact	: Ben Vandenberghe
-
-****************************************************************************
 Revision History:
 
 DATE		VERSION		AUTHOR			COMMENTS
@@ -61,7 +23,7 @@ namespace SLCSMASDynamicDelete
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
 	using Skyline.DataMiner.Net.Messages.SLDataGateway;
-
+	using Skyline.DataMiner.Utils.ServiceManagement.Common.IAS;
 	using SLC_SM_AS_DynamicDelete;
 
 	/// <summary>
@@ -79,6 +41,18 @@ namespace SLCSMASDynamicDelete
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
+			/*
+			* Note:
+			* Do not remove the commented methods below!
+			* The lines are needed to execute an interactive automation script from the non-interactive automation script or from Visio!
+			*
+			* engine.ShowUI();
+			*/
+			if (engine.IsInteractive)
+			{
+				engine.FindInteractiveClient("Failed to run script in interactive mode", 1);
+			}
+
 			try
 			{
 				RunSafe(engine);
@@ -106,7 +80,7 @@ namespace SLCSMASDynamicDelete
 			}
 			catch (Exception e)
 			{
-				engine.ExitFail("Run|Something went wrong: " + e);
+				engine.ShowErrorDialog(e);
 			}
 		}
 
