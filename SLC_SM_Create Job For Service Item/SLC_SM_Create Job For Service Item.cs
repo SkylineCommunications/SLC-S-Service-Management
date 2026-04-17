b50c81d78ddc5cd7351cbb0dbffb07e1a02a719d
Samuel Duarte <samuel.duarte@skyline.be>
2026-04-17 12:22:04 +0100

    Demo pass parameters values to MediaOps Jobs

--- SLC_SM_Create Job For Service Item/SLC_SM_Create Job For Service Item.cs
+++ SLC_SM_Create Job For Service Item/SLC_SM_Create Job For Service Item.cs
@@ -48,23 +48,36 @@ DATE        VERSION        AUTHOR            COMMENTS
 13/03/2025    1.0.0.1        RME, Skyline    Initial version
 ****************************************************************************
 */
+
 namespace SLCSMCreateJobForServiceItem
 {
 	using System;
+	using System.Collections.Generic;
 	using System.Linq;
+
 	using DomHelpers.SlcWorkflow;
+
+	using Newtonsoft.Json;
+
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
 	using Skyline.DataMiner.Net.Messages.SLDataGateway;
-	using Skyline.DataMiner.Net.ResourceManager.Objects;
+	using Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations;
 	using Skyline.DataMiner.ProjectApi.ServiceManagement.API.ServiceManagement;
 	using Skyline.DataMiner.ProjectApi.ServiceManagement.SDM;
+	using Skyline.DataMiner.Utils.MediaOps.Common;
+	using Skyline.DataMiner.Utils.MediaOps.Common.DOM.Applications.Workflow;
 	using Skyline.DataMiner.Utils.MediaOps.Common.IOData.Scheduling.Scripts.JobHandler;
 	using Skyline.DataMiner.Utils.MediaOps.Helpers.Relationships;
+	using Skyline.DataMiner.Utils.MediaOps.Helpers.ResourceStudio;
 	using Skyline.DataMiner.Utils.MediaOps.Helpers.Workflows;
 	using Skyline.DataMiner.Utils.ServiceManagement.Common.Extensions;
 	using Skyline.DataMiner.Utils.ServiceManagement.Common.IAS;
+
 	using static DomHelpers.SlcServicemanagement.SlcServicemanagementIds.Behaviors.Service_Behavior;
+
+	using DomApplications = Skyline.DataMiner.Utils.MediaOps.Common.DOM.Applications;
+	using ServiceModels = Skyline.DataMiner.ProjectApi.ServiceManagement.API.ServiceManagement.Models;
 
 	/// <summary>
 	///     Represents a DataMiner Automation script.
@@ -114,7 +127,7 @@ namespace SLCSMCreateJobForServiceItem
 			}
 		}
 
-		private static void UpdateState(DataHelperService srvHelper, Models.Service service)
+		private static void UpdateState(DataHelperService srvHelper, ServiceModels.Service service)
 		{
 			// If all items are in progress -> move to In Progress
 			if (!service.ServiceItems.All(x => !String.IsNullOrEmpty(x.ImplementationReference) && x.ImplementationReference != ReferenceUnknown))
@@ -138,7 +151,7 @@ namespace SLCSMCreateJobForServiceItem
 			}
 		}
 
-		private void AddOrUpdateServiceItemToInstance(DataHelperService helper, Models.Service instance, Models.ServiceItem newSection, string oldLabel)
+		private void AddOrUpdateServiceItemToInstance(DataHelperService helper, ServiceModels.Service instance, ServiceModels.ServiceItem newSection, string oldLabel)
 		{
 			var oldItem = instance.ServiceItems.FirstOrDefault(x => x.Label == oldLabel);
 			if (oldItem != null)
@@ -157,7 +170,7 @@ namespace SLCSMCreateJobForServiceItem
 			UpdateState(helper, instance);
 		}
 
-		private CreateJobAction CreateJobConfiguration(Models.Service instance, Models.ServiceItem serviceItemsSection, Workflow workflow)
+		private CreateJobAction CreateJobConfiguration(ServiceModels.Service instance, ServiceModels.ServiceItem serviceItemsSection, Skyline.DataMiner.Utils.MediaOps.Helpers.Workflows.Workflow workflow)
 		{
 			return new CreateJobAction
 			{
@@ -171,7 +184,7 @@ namespace SLCSMCreateJobForServiceItem
 			};
 		}
 
-		private void CreateLink(IEngine engine, Models.Service instance, JobsInstance job)
+		private void CreateLink(IEngine engine, ServiceModels.Service instance, JobsInstance job)
 		{
 			var relationshipHelper = new RelationshipsHelper(engine);
 
@@ -182,7 +195,7 @@ namespace SLCSMCreateJobForServiceItem
 			relationshipHelper.CreateLink(linkDetailsConfiguration);
 		}
 
-		private LinkConfiguration CreateLinkDetailsConfiguration(Models.Service instance, JobsInstance job, Guid serviceObjectType, Guid jobObjectType)
+		private LinkConfiguration CreateLinkDetailsConfiguration(ServiceModels.Service instance, JobsInstance job, Guid serviceObjectType, Guid jobObjectType)
 		{
 			var linkConfiguration = new LinkConfiguration
 			{
@@ -243,18 +256,18 @@ namespace SLCSMCreateJobForServiceItem
 
 			var dataHelperService = new DataHelperService(engine.GetUserConnection());
 			var instance = dataHelperService.Read(ServiceExposers.Guid.Equal(domId)).FirstOrDefault()
-			               ?? throw new InvalidOperationException($"No Service exists with ID '{domId}'");
+						   ?? throw new InvalidOperationException($"No Service exists with ID '{domId}'");
 			var serviceItemsSection = instance.ServiceItems.SingleOrDefault(s => s.Label == label)
-			                          ?? throw new InvalidOperationException($"Could not find the service item section with label '{label}'");
+									  ?? throw new InvalidOperationException($"Could not find the service item section with label '{label}'");
 
-			if (!engine.DomModelExists(SlcWorkflowIds.ModuleId, new[] {SlcWorkflowIds.Sections.WorkflowInfo.Id.Id}))
+			if (!engine.DomModelExists(SlcWorkflowIds.ModuleId, new[] { SlcWorkflowIds.Sections.WorkflowInfo.Id.Id }))
 			{
 				throw new InvalidOperationException("The Media Ops solution needs to be installed to use this feature. The '(slc)workflow' DOM model is required but not found on the system.");
 			}
 
 			var workflowHelper = new WorkflowHelper(engine);
 			var workflow = workflowHelper.GetAllWorkflows().FirstOrDefault(x => x.Name == serviceItemsSection.DefinitionReference)
-			               ?? throw new InvalidOperationException($"No Workflow found on the system with name '{serviceItemsSection.DefinitionReference}'");
+						   ?? throw new InvalidOperationException($"No Workflow found on the system with name '{serviceItemsSection.DefinitionReference}'");
 
 			if (instance.EndTime.HasValue && instance.EndTime.Value < DateTime.UtcNow)
 			{
@@ -294,6 +307,8 @@ namespace SLCSMCreateJobForServiceItem
 			var domWorkflowHelper = new DomHelper(engine.SendSLNetMessages, SlcWorkflowIds.ModuleId);
 			var job = FindJob(domWorkflowHelper, jobId);
 
+			UpdateJobWithParameterConfigurations(engine, jobId, instance);
+
 			if (job.Status == SlcWorkflowIds.Behaviors.Job_Behavior.StatusesEnum.Draft)
 			{
 				var transitionJobToTentativeInputData = new ExecuteJobAction
@@ -315,10 +330,169 @@ namespace SLCSMCreateJobForServiceItem
 			AddOrUpdateServiceItemToInstance(dataHelperService, instance, serviceItemsSection, label);
 		}
 
+		private void UpdateJobWithParameterConfigurations(IEngine engine, Guid jobId, ServiceModels.Service instance)
+		{
+			WorkflowHandler worflowHandler = new DomApplications.Workflow.WorkflowHandler(engine);
+			ResourceStudioHelper resourceStudioHelper = new ResourceStudioHelper(engine);
+			Guid domConfigurationId = GetDomConfigurationIdFromJob(engine, worflowHandler, jobId);
+
+			if (domConfigurationId == Guid.Empty)
+			{
+				return;
+			}
+
+			var domConfiguration = worflowHandler.GetConfigurationByDomInstanceId(domConfigurationId);
+
+			if (domConfiguration == null || domConfiguration.ProfileParameterValues.Count == 0)
+			{
+				return;
+			}
+
+			var parameterIds = domConfiguration.ProfileParameterValues.Select(x => x.ProfileParameterId).ToList();
+			if (parameterIds.Count == 0)
+			{
+				return;
+			}
+
+			var parameters = resourceStudioHelper.GetParameters(parameterIds);
+			var jobParameters = domConfiguration.ProfileParameterValues.Select(
+				o
+				=> new
+				{
+					ParameterValue = new Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ProfileParameterValue
+					{
+						ProfileParameterId = o.ProfileParameterId,
+						StringValue = o.StringValue,
+						DoubleValue = o.DoubleValue,
+					},
+					parameters.Configurations.FirstOrDefault(p => p.Id == o.ProfileParameterId)?.Name,
+				});
+
+			engine.Log($"Job Param:\n{JsonConvert.SerializeObject(jobParameters)}");
+			var serviceParams = instance.ServiceConfiguration.Parameters.Select(p => p.ConfigurationParameter).ToList();
+			foreach (var profile in instance.ServiceConfiguration.Profiles)
+			{
+				serviceParams.AddRange(profile.Profile.ConfigurationParameterValues);
+			}
+
+			var configurationParameters = GetFilteredConfigurationParameters(engine, serviceParams);
+
+			var serviceParamsWithConfigParam = serviceParams.Select(sp => new
+			{
+				ConfigurationParameterValue = sp,
+				ConfigurationParameter = configurationParameters.FirstOrDefault(cp => cp.ID == sp.ConfigurationParameterId),
+			}).ToList();
+
+			List<Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ProfileParameterValue> overriddenValues = new List<Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ProfileParameterValue>();
+
+			engine.Log($"Service params:\n{JsonConvert.SerializeObject(serviceParamsWithConfigParam.Select(p => new { p.ConfigurationParameter.Name }))}");
+
+			foreach (var param in jobParameters)
+			{
+				var serviceParam = serviceParamsWithConfigParam.FirstOrDefault(p =>
+				{
+					if (p.ConfigurationParameter == null)
+					{
+						return false;
+					}
+
+					string paramName = p.ConfigurationParameter.Name;
+
+					if (paramName == "Frequency")
+					{
+						paramName = "IRD input frequency";
+					}
+
+					if (paramName == "Symbol Rate")
+					{
+						paramName = "IRD symbol rate";
+					}
+
+					return param.Name == paramName;
+				});
+
+				if (serviceParam == null)
+				{
+					overriddenValues.Add(param.ParameterValue);
+					continue;
+				}
+
+				engine.Log($"Found matching service parameter for job parameter '{param.Name}':\n{JsonConvert.SerializeObject(serviceParam)}");
+
+				if (serviceParam.ConfigurationParameterValue.Type == DomHelpers.SlcConfigurations.SlcConfigurationsIds.Enums.Type.Number)
+				{
+					engine.Log($"Overriding parameter '{param.Name}' with value '{serviceParam.ConfigurationParameterValue.DoubleValue}'");
+					overriddenValues.Add(new Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ProfileParameterValue
+					{
+						ProfileParameterId = param.ParameterValue.ProfileParameterId,
+						DoubleValue = serviceParam.ConfigurationParameterValue.DoubleValue,
+					});
+				}
+				else
+				{
+					engine.Log($"Overriding parameter '{param.Name}' with value '{serviceParam.ConfigurationParameterValue.StringValue}'");
+					overriddenValues.Add(new Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ProfileParameterValue
+					{
+						ProfileParameterId = param.ParameterValue.ProfileParameterId,
+						StringValue = serviceParam.ConfigurationParameterValue.StringValue,
+					});
+				}
+			}
+
+			engine.Log($"Overridden values:\n{JsonConvert.SerializeObject(overriddenValues)}");
+
+			if (overriddenValues.Count > 0)
+			{
+				var editConfigurationAction = new Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.EditConfigurationAction
+				{
+					Context = new Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ConfigurationContext
+					{
+						DomInstanceId = jobId,
+						Target = Skyline.DataMiner.Utils.MediaOps.Common.IOData.Workflows.Scripts.ConfigurationHandler.ConfigurationTarget.Job,
+					},
+					DomConfigurationId = domConfiguration.InstanceId,
+					OverriddenValues = overriddenValues,
+				};
+				editConfigurationAction.SendToConfigurationHandler(engine);
+			}
+		}
+
 		private void TrySetMonitoringSettingsForJob(JobsInstance job)
 		{
 			job.MonitoringSettings.AtJobStart = SlcWorkflowIds.Enums.Atjobstart.CreateServiceAtWorkflowStart;
 			job.MonitoringSettings.AtJobEnd = SlcWorkflowIds.Enums.Atjobend.DeleteServiceIfOneExists;
+		}
+
+		private Guid GetDomConfigurationIdFromJob(IEngine engine, WorkflowHandler workflowHandler, Guid domInstanceId)
+		{
+			var domJob = workflowHandler.GetJobByDomInstanceId(domInstanceId);
+			if (domJob == null)
+			{
+				engine.Log($"No job available with ID '{domInstanceId}'.");
+				return Guid.Empty;
+			}
+
+			return domJob.JobExecution?.ConfigurationId ?? Guid.Empty;
+		}
+
+		private List<Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations.Models.ConfigurationParameter> GetFilteredConfigurationParameters(IEngine engine, List<Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations.Models.ConfigurationParameterValue> serviceParams)
+		{
+			FilterElement<Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations.Models.ConfigurationParameter> filterConfigParams =
+				new ORFilterElement<Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations.Models.ConfigurationParameter>();
+			var usedConfigurationParameterIds = serviceParams
+				.Where(x => x?.ConfigurationParameterId != null && x.ConfigurationParameterId != Guid.Empty)
+				.Select(x => x.ConfigurationParameterId)
+				.ToList()
+												?? new List<Guid>();
+			foreach (Guid guid in usedConfigurationParameterIds)
+			{
+				filterConfigParams = filterConfigParams.OR(ConfigurationParameterExposers.Guid.Equal(guid));
+			}
+
+			var configurationParameters = !filterConfigParams.isEmpty()
+				? new DataHelperConfigurationParameter(engine.GetUserConnection()).Read(filterConfigParams)
+				: new List<Skyline.DataMiner.ProjectApi.ServiceManagement.API.Configurations.Models.ConfigurationParameter>();
+			return configurationParameters;
 		}
 	}
 }
